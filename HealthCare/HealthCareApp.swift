a12b89286d126ef345500489bc157cde8b7d8d55
Michał Babicz <kiwuzx@gmail.com>
2023-03-28 22:28:50 +0200

    Initial Commit

---
+++ HealthCare/HealthCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCareApp.swift
+//  HealthCare
+//
+//  Created by kz on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
