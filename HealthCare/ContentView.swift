51878dda9ab411a3fb2484d76838039f91ccf06b
Michał Babicz <kiwuzx@gmail.com>
2023-03-29 23:11:38 +0200

    HealthKit integration

--- HealthCare/ContentView.swift
+++ HealthCare/ContentView.swift
@@ -6,21 +6,49 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    @State var stepsCount: Int? = nil
+    let healthStore = HKHealthStore()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let steps = stepsCount {
+                Text("steps: \(steps)")
+            } else {
+                Text("Loading...")
+            }
         }
-        .padding()
+        .onAppear(perform: fetchTodayStepsCount)
     }
-}
+    
+    func fetchTodayStepsCount() {
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let calendar = Calendar.current
+        let now = Date()
+        let startOfDay = calendar.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            guard let result = result, let sum = result.sumQuantity() else {
+                if let error = error {
+                    print("Eror: \(error.localizedDescription)")
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.stepsCount = Int(sum.doubleValue(for: .count()))
+            }
+        }
+        
+        healthStore.requestAuthorization(toShare: [], read: [stepsQuantityType]) { (success, error) in
+            if success {
+                healthStore.execute(query)
+            } else {
+                print("User doesn't allow to share healthkit data")
+            }
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
 }
