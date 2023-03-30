8771be2708ffdcd89f3d970a43ec05ecfcee2f7b
Michał Babicz <kiwuzx@gmail.com>
2023-03-30 17:58:35 +0200

    missing entitlements

--- HealthCare/Views/HealthView.swift
+++ HealthCare/Views/HealthView.swift
@@ -15,14 +15,14 @@ struct HealthView: View {
             VStack {
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Today's Steps")
+                        Text("Today's steps")
                             .font(.headline)
                         Text("\(healthManager.stepsCount)")
                             .font(.title)
                     }
                     Spacer()
                     VStack(alignment: .trailing) {
-                        Text("Today's Calories Burned")
+                        Text("Today's calories burned")
                             .font(.headline)
                         Text("\(healthManager.caloriesBurned, specifier: "%.1f")")
                             .font(.title)
@@ -32,14 +32,14 @@ struct HealthView: View {
                 Divider()
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Last 7 Days Steps")
+                        Text("Last 7 days steps avg.")
                             .font(.headline)
                         Text("\(healthManager.averageStepsLast7Days)")
                             .font(.title)
                     }
                     Spacer()
                     VStack(alignment: .trailing) {
-                        Text("Last 7 Days Calories Burned")
+                        Text("Last 7 days calories burned avg.")
                             .font(.headline)
                         Text("\(healthManager.averageCaloriesBurnedLast7Days)")
                             .font(.title)
@@ -49,14 +49,14 @@ struct HealthView: View {
                 Divider()
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Last 30 Days Steps")
+                        Text("Last 30 days steps avg.")
                             .font(.headline)
                         Text("\(healthManager.averageStepsLast30Days)")
                             .font(.title)
                     }
                     Spacer()
                     VStack(alignment: .trailing) {
-                        Text("Last 30 Days Calories Burned")
+                        Text("Last 30 days calories burned avg.")
                             .font(.headline)
                         Text("\(healthManager.averageCaloriesBurnedLast30Days)")
                             .font(.title)
